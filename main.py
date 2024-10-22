0a0150a94549a2f4dc959489765aa380170dc3a4
juankaruna <juankaruna@gmail.com>
2024-10-21 18:04:39 -0600

    implementation of OOP

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
-import requests
-import json
 import pandas as pd
 
-from bs4 import BeautifulSoup
 from config import Config
-#from helpers import unwrap_json
 from scraping import ScrapingPage, ScrapingProperty
-
-#LAYERS_TO_UNWRAP = [0, 'props', 'pageProps', 'initialState', 'propertyData', 'properties']
 
 domain = Config.DOMAIN
 url_page = Config.URL_PAGE
-#headers = Config.HEADERS
 
 properties_columns = ['Name', 'Price', 'Room', 'Bathroom', 'Parking Lots', 'Description', 'Area']
 properties_dic = {
@@ -37,68 +30,11 @@ if __name__ == '__main__':
     print(properties_dic)
     
 
-def main():
-    session = requests.session()
-    response = session.get(domain + url_page, headers=headers)
+def convert_to_csv():
+    df = pd.DataFrame(properties_dic, columns=properties_columns)
 
-    if response.status_code == 200:
-        content = response.text
+    print("Original DataFrame:")
+    print(df)
+    print('Data from Users.csv:')
 
-        page_name = url_page.split('/')[-1]
-
-        #with open(f'assets/templates/{page_name}.text', 'w+') as file:
-        #    file.write(content)
-
-        #    print('Tamplate generado con exito!')
-
-        soup = BeautifulSoup(content, 'html.parser')
-
-        properties_json = [json.loads(element.text) for element in soup.find_all('script', id='__NEXT_DATA__')]
-        
-        properties = unwrap_json(properties_json, LAYERS_TO_UNWRAP)
-        
-        for property in properties:
-            url_property = property['canonical']
-            response_property = session.get(domain + url_property, headers=headers)
-            
-            if response_property.status_code == 200:
-                content = response_property.text
-                url_property = url_property.split('/')[-1]
-
-                #with open(f'assets/templates/{url_property}.text', 'w+') as file:
-                #    file.write(content)
-
-                #    print('Tamplate generado con exito!')
-
-                soup = BeautifulSoup(content, 'html.parser')
-
-                properties_dic['Name'].append(soup.find('h1', class_='font-bold text-gray-700 capitalize text-lg md:text-xl').text)
-                properties_dic['Price'].append(soup.find('p', class_='text-2xl m-0').text)
-                properties_dic['Room'].append(soup.find('img', src='/static/img/property_description/rooms.svg').next_sibling.text) #Validar que si exista img con ese src
-                properties_dic['Bathroom'].append(soup.find('img', src='/static/img/property_description/bathrooms.svg').next_sibling.text) #Validar que si exista img con ese src
-                #properties_dic['Toilet'].append(soup.find('img', src='/static/img/property_description/halfBathrooms.svg').next_sibling.text) #Validar que si exista img con ese src
-                properties_dic['Parking Lots'].append(soup.find('img', src='/static/img/property_description/parking_lots.svg').next_sibling.text) #Validar que si exista img con ese src
-                properties_dic['Description'].append([element.text for element in soup.find('div', class_='prose max-w-screen-md').children]) #Validar que si exista img con ese src
-                properties_dic['Area'].append(soup.find('p', class_='mb-0').text)
-
-                if bandera_chigona == 5:
-
-                    break
-
-                bandera_chigona = bandera_chigona + 1
-
-            else:
-                print(f'No se logro acceder al sitio! Respuesta del Server:{response_property}')       
-        
-        df = pd.DataFrame(properties_dic, columns=properties_columns)
-
-        print("Original DataFrame:")
-        print(df)
-        print('Data from Users.csv:')
-
-        df.to_csv('assets/properties.csv', header=True)
-
-    else:
-        print(f'No se logro acceder al sitio! Respuesta del Server:{response.text}')
-
-        
+    df.to_csv('assets/properties.csv', header=True)        
